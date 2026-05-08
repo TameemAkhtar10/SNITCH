67ca6e5d3a9b8d16691795f8f83f4be85d6ceff2
tameem akhtar <akhtartamimm@gmail.com>
2026-05-09 00:17:18 +0530

    fix case sensitive import

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/app.store.js'
-import './app/app.css'
+import './app/App.css'
 import App from './app/App.jsx'
 
 createRoot(document.getElementById('root')).render(
