59a876753abdc399e4daaf416a4a4a5192e6ce91
tameem akhtar <akhtartamimm@gmail.com>
2026-05-09 14:33:26 +0530

    fix case sensitive import

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -125,7 +125,7 @@ export const googleCallback = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
-        const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173'
+        const frontendURL ='https://snitch-aukv.onrender.com/login'
         const redirectTo = req.query.state || '/home'
         return res.redirect(`${frontendURL}/auth/google/success?token=${token}&redirectTo=${encodeURIComponent(redirectTo)}`);
     } catch (error) {
