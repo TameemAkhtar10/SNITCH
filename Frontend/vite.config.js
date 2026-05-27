aab965f5b51ae184687f30d0f390a41081696ba6
tameem akhtar <akhtartamimm@gmail.com>
2026-05-27 11:42:24 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: true,
     minify: 'esbuild',
     target: 'es2015',
   },
