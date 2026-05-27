483a9b37c38b23e61b78f82376aa87f963173b1e
tameem akhtar <akhtartamimm@gmail.com>
2026-05-27 11:15:43 +0530

    FINALLY ADD ANIMATION

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'esbuild',
+    target: 'es2015',
   },
   optimizeDeps: {
     include: ['gsap'],
